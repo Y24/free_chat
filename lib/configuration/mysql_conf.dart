e9e733f4e9a139dbce4f5874608806bfed21b247
y24 <githuby24@gmail.com>
2019-10-24 10:05:21 +0800

    Test .gitignore.

--- lib/configuration/mysql_conf.dart
+++ lib/configuration/mysql_conf.dart
@@ -2,9 +2,8 @@ abstract class MysqlConf {
   static final String host = 'y24.org.cn';
   static final int port = 3306;
   static final String user = 'y24';
-  static final String password = '*';
+  static final String password = '***';
   static final String db = 'free_chat';
-
   /// The timeout for connecting to the database and for all database operations.
   static final Duration timeout = const Duration(seconds: 2);
 }
